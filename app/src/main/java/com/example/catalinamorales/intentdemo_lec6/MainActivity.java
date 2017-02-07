b616f032e8b875aeeded63362bdc6949eff20d10
Catmoralesrod <cdcmr.2016@gmail.com>
2017-02-07 09:25:26 -0500

    code update

--- app/src/main/java/com/example/catalinamorales/intentdemo_lec6/MainActivity.java
+++ app/src/main/java/com/example/catalinamorales/intentdemo_lec6/MainActivity.java
@@ -11,11 +11,12 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import static com.example.catalinamorales.intentdemo_lec6.Utils.Utils.AGE_MSG_KEY;
+import static com.example.catalinamorales.intentdemo_lec6.Utils.Utils.NAME_MSG_KEY;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
-    private EditText editText1;
+    private EditText editText1, editText2;
     private ListView lsView;
 
 
@@ -34,13 +35,15 @@ public class MainActivity extends AppCompatActivity {
         lsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                view.setSelected(true);
                 Intent intent = new Intent(MainActivity.this , Main2Activity.class);
                 intent.putExtra(AGE_MSG_KEY, "Item is : " +myItems[i]);
                 startActivity(intent);
             }
         });
 
-        editText1=(EditText) findViewById(R.id.edttxt_ma1);
+        editText1=(EditText) findViewById(R.id.edittxt_ma1);
+        editText2=(EditText) findViewById(R.id.edittxt_ma2);
         btn1 =(Button)findViewById(R.id.btn1_ma1);
         btn1.setOnClickListener(new MyLstnr());
 
@@ -52,6 +55,11 @@ class MyLstnr implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        Intent intent2 = new Intent(MainActivity.this, Main2Activity.class);
+        intent2.putExtra(AGE_MSG_KEY,"Your age is " + Integer.parseInt(editText1.getText().toString()));
+        intent2.putExtra(NAME_MSG_KEY,"Your name is " +editText2.getText().toString());
+        startActivity(intent2);
+
 
     }
 }
--- app/src/main/java/com/example/catalinamorales/intentdemo_lec6/Utils/Utils.java
+++ app/src/main/java/com/example/catalinamorales/intentdemo_lec6/Utils/Utils.java
@@ -7,4 +7,5 @@ package com.example.catalinamorales.intentdemo_lec6.Utils;
 public class Utils {
 
     public final static String AGE_MSG_KEY="Msg_key";
+    public final static String NAME_MSG_KEY="Name_key";
 }
